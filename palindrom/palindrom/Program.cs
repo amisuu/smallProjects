b3ad9d1a22194d1c39a2bb840da33a71f2aa50ca
Patryk <liver13@wp.pl>
2020-06-22 16:34:50 +0200

    add palindrom

---
+++ palindrom/palindrom/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            palindrom();
+        }
+
+        public static void palindrom()
+        {
+            
+            int ile = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < ile; i++)
+            {
+                int count = 0;
+                int odwrotnie = 0;
+                string tekst = Console.ReadLine();
+                int liczba = int.Parse(tekst);
+                string tekst2 = "";
+                
+                do
+                {
+                    for (int j = tekst.Length - 1; j >= 0; j--)
+                    {
+                        tekst2 += tekst[j];
+                    }
+                    odwrotnie = int.Parse(tekst2);                    
+                    if (odwrotnie == liczba)
+                    {
+                        
+                        break;
+                    }
+                    liczba += odwrotnie;
+                    count++;
+                    tekst = liczba.ToString();
+                    tekst2 = "";
+                } while (true);
+                Console.WriteLine($"Liczba {liczba} jest palindormem, wykonalo sie {count} powtorzen");
+            }
+        }
+    }
+}
